c33a94008d8d85adb9cdb96a878b71c17e48822e
kamilaut <kamila.utyukova@gmail.com>
2022-12-26 11:42:05 +0100

    replaced movies array with an empty array as the initial value

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,45 +4,7 @@ import { MovieView } from "../movie-view/movie-view";
 
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Shoplifters",
-            image:
-                "https://cdn.theatlantic.com/thumbor/eqrBtLW_OyfF6AZhu9H-jrkJaa4=/0x0:1000x563/976x549/media/img/mt/2018/11/shop/original.jpg",
-            author: "Hirokazu Kore-eda",
-            year: "2018",
-            description: "Osamu, a shoplifter who lives with his family, finds Yuri, a little girl, freezing in the cold and takes her home. Osamu decides to adopt Yuri when he learns about her parents."
-        },
-        {
-            id: 2,
-            title: "Triangle of Sadness",
-            image:
-                "https://img.zeit.de/2022/41/wir-sind-verdammt-oft-gute-menschen-bild-1/wide__1300x731",
-            author: "Ruben Östlund",
-            year: "2022",
-            description: ""
-
-        },
-        {
-            id: 3,
-            title: "Broker",
-            image:
-                "https://ychef.files.bbci.co.uk/976x549/p0c9fwk1.jpg",
-            author: "Hirokazu Koreeda",
-            year: "2022",
-            description: ""
-        },
-        {
-            id: 4,
-            title: "My Night at Maud's",
-            image:
-                "https://www.filmlinc.org/wp-content/uploads/2016/08/mynightatmauds-1-1600x900-c-default.jpg",
-            author: "Éric Rohmer",
-            year: "1969",
-            description: ""
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
     if (selectedMovie) {
