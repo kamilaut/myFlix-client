5db59de9eb92ca02153e26a446af16a1b8ebf59f
kamilaut <kamila.utyukova@gmail.com>
2023-01-22 14:16:35 +0100

    added routes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,8 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { Row, Col, Container, Button } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+
 
 import "./main-view.scss";
 
@@ -16,38 +18,38 @@ export const MainView = () => {
     const [user, setUser] = useState(storedUser ? storedUser : null);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
-    useEffect(() => {
-        if (!token) {
-            return;
-        }
-        fetch("https://mirror-stage.herokuapp.com/movies", {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                //data is the movies array you get from the movie_api
-                //storing it in the movies state using setMovies:
-                setMovies(data)
-            });
-    }, [token]);
+    // useEffect(() => {
+    //     if (!token) {
+    //         return;
+    //     }
+    //     fetch("https://mirror-stage.herokuapp.com/movies", {
+    //         headers: { Authorization: `Bearer ${token}` }
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             //data is the movies array you get from the movie_api
+    //             //storing it in the movies state using setMovies:
+    //             setMovies(data)
+    //         });
+    // }, [token]);
 
-    if (!user) {
-        return (
-            <>
-                <Col md={10} >
-                    <LoginView onLoggedIn={(user, token) => {
+    // if (!user) {
+    //     return (
+    //         <>
+    //             <Col md={10} >
+    //                 <LoginView onLoggedIn={(user, token) => {
 
-                        setUser(user);
-                        setToken(token);
+    //                     setUser(user);
+    //                     setToken(token);
 
-                    }} />
-                    or
-                    <SignupView />
-                </Col>
-            </>
-        );
-    }
+    //                 }} />
+    //                 or
+    //                 <SignupView />
+    //             </Col>
+    //         </>
+    //     );
+    // }
 
     if (selectedMovie) {
         return (
@@ -62,34 +64,114 @@ export const MainView = () => {
         );
     }
 
-    if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-    }
+    // if (movies.length === 0) {
+    //     return <div>The list is empty!</div>;
+    // }
 
     return (
-        <Row className="justify-content-md-center">
-            <Button
-                className="btn-logout"
-                onClick={() => {
-                    setUser(null);
-                    setToken(null);
-                    localStorage.clear();
-                }}
-            >
-                Logout
-            </Button>
-            {movies.map((movie) => (
-                <Col className="mb-3" key={movie._id} md={3}>
-                    <MovieCard key={movie._id}
-                        movie={movie}
-                        onMovieClick={(newSelectedMovie) => {
-                            setSelectedMovie(newSelectedMovie);
-                            console.log(selectedMovie)
-                        }}
+        <BrowserRouter>
+
+            <Row className="justify-content-md-center">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Link to={`/login`}>
+                                    <Button variant="link">Login</Button>
+                                </Link>
+
+                            </>
+                        }
                     />
-                </Col>
-            ))}
-        </Row>
+
+                    <Route
+                        path="/signup"
+                        element={
+                            <>
+                                {/* {user ? (
+                                    <Navigate to="/" />
+                                ) : ( */}
+                                <Col md={10}>
+                                    <SignupView />
+                                </Col>
+                                {/* )} */}
+                            </>
+
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <>
+                                {/* {user ? (
+                                    <Navigate to="/" />
+                                ) : ( */}
+                                <Col md={10}>
+                                    <LoginView />
+                                </Col>
+                                {/* )} */}
+                            </>
+
+                        }
+                    />
+                    <Route
+                        path="/movies"
+                        element={
+                            <>
+                                {/* {user ? (
+                                    <Navigate to="/" />
+                                ) : ( */}
+                                <Col md={10}>
+                                    <MovieView />
+                                </Col>
+                                {/* )} */}
+                            </>
+
+                        }
+                    />
+                    <Route
+                        path="/movies/:Title"
+                        element={
+                            <>
+                                {/* {user ? (
+                                    <Navigate to="/" />
+                                ) : ( */}
+                                <Col md={10}>
+                                    <MovieView />
+                                </Col>
+                                {/* )} */}
+                            </>
+
+                        }
+                    />
+
+                </Routes>
+
+                {/* {movies.map((movie) => (
+                    <Col className="mb-3" key={movie._id} md={3}>
+                        <MovieCard key={movie._id}
+                            movie={movie}
+                            onMovieClick={(newSelectedMovie) => {
+                                setSelectedMovie(newSelectedMovie);
+                                console.log(selectedMovie)
+                            }}
+                        />
+                    </Col>
+                ))}
+                <Button
+                    className="btn-logout"
+                    onClick={() => {
+                        setUser(null);
+                        setToken(null);
+                        localStorage.clear();
+                    }}
+                >
+                    Logout
+                </Button> */}
+            </Row>
+        </BrowserRouter>
+
     );
 
 };
